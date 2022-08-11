51e4e257bf84339ed092253b528ed0fb109f88ac
Aya Mohsen <101042082+ayaahmohsenn@users.noreply.github.com>
2022-08-11 12:24:53 +0200

    Add files via upload

---
+++ Class.h
@@ -0,0 +1,28 @@
+/* class.h */
+
+#define ADMINUSER "Teacher"
+#define ADMINPASSWORD "teacher123"
+
+#define USER "Student"
+#define USERPASSWORD "student123"
+
+#define FINE 500
+
+int addBook();
+
+int deleteBook();
+
+int updateBook();
+
+int listOfAllBooks();
+
+int borrowBook();
+
+int returnBook();
+
+int adminLogin();
+
+int userLogin();
+    return 0;
+}
+
--- main.cpp
+++ main.cpp
@@ -320,38 +320,5 @@ int adminLogin ()
 int userLogin ()
 {
 }
-?
 
 
-
-
-
-Step 2: Create one more file as class.h
-
-/* class.h */
-
-#define ADMINUSER "Teacher"
-#define ADMINPASSWORD "teacher123"
-
-#define USER "Student"
-#define USERPASSWORD "student123"
-
-#define FINE 500
-
-int addBook();
-
-int deleteBook();
-
-int updateBook();
-
-int listOfAllBooks();
-
-int borrowBook();
-
-int returnBook();
-
-int adminLogin();
-
-int userLogin();
-    return 0;
-}
